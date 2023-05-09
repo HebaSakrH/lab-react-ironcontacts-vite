c77167af337a98f415e1ef204169fc8f1f930a3f
HebaSakrH <128267375+HebaSakrH@users.noreply.github.com>
2023-05-09 20:10:29 +0300

    iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,45 @@ import contacts from './contacts.json'
 import './App.css'
 
 function App() {
-  const [firstFive, setFirstFive] = useState(contacts.slice(0,5))
-console.log(firstFive)
+  const [firstFive, setFirstFive] = useState(contacts.slice(0, 5));
+
+  function handleAddRandom() {
+    const randomIndex = Math.floor(Math.random() * contacts.length);
+    const randomContact = contacts[randomIndex];
+    setFirstFive([...firstFive, randomContact]);
+  }
   return (
     <div className="App">
-         <h1>IronContacts</h1>
+      <h1>IronContacts</h1>
+      <div className="btn-container">
+        <button onClick={handleAddRandom}>Add Random Contact</button>
+      </div>
       <table>
         <thead>
           <tr>
             <th>Picture</th>
             <th>Name</th>
             <th>Popularity</th>
+            <th>Won Oscar</th> 
+            <th>Won Emmy</th> 
           </tr>
         </thead>
         <tbody>
-          {firstFive.map((contacts) => (
-            <tr key={contacts.id}>
+          {firstFive.map((contact) => (
+            <tr key={contact.id}>
               <td>
-                <img src={contacts.pictureUrl} alt={contacts.name} />
+                <img src={contact.pictureUrl} alt={contact.name} />
               </td>
-              <td>{contacts.name}</td>
-              <td>{contacts.popularity.toFixed(2)}</td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar ? <span role="img" aria-label="Oscar trophy">🏆</span> : null}</td> 
+              <td>{contact.wonEmmy ? <span role="img" aria-label="Emmy trophy">🏆</span> : null}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
   );
-  
-
-
 }
 
 export default App
